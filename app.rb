e75f2c6d7ba4516e681cc0338a8ff6bb66d333cb
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-01 17:56:16 +0000

    Refactored app.rb for readability

--- app.rb
+++ app.rb
@@ -6,5 +6,5 @@ sql = 'SELECT id, name, avg_cook_time, rating FROM recipes;'
 result = DatabaseConnection.exec_params(sql, [])
 
 result.each do |record|
-  p record
+  p "#{record['id']}. #{record['name']} - Cooking Time: #{record['avg_cook_time']} mins. Rating (1-5): #{record['rating']}"
 end  
